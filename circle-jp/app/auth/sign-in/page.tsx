04651dc8b513b82ce46cb18a7ceb69f82853675d
Nagira16 <tsuji0032@gmail.com>
2025-11-05 01:04:13 -0800

    Fix-Button-Styles

--- circle-jp/app/auth/sign-in/page.tsx
+++ circle-jp/app/auth/sign-in/page.tsx
@@ -13,7 +13,10 @@ function SignIn() {
       <main className="border border-inherit w-[600px] h-[370px] rounded-2xl p-6 z-10 backdrop-blur-xl space-y-10">
         <h4 className="text-center text-4xl">Sign In</h4>
 
-        <form action="" className="mx-auto w-[400px]">
+        <form
+          action=""
+          className="mx-auto w-[400px] h-60 flex flex-col justify-between"
+        >
           <div className="space-y-3">
             <div>
               <legend>Email</legend>
@@ -34,14 +37,14 @@ function SignIn() {
             </div>
           </div>
 
-          <div className="flex justify-between items-center pt-[94px] text-[16px]">
+          <div className="flex justify-between items-center pt-[94px] text-[17px] text-center">
             <Link
               href={"/auth/sign-up"}
-              className="bg-white text-black py-1 px-2 rounded-2xl"
+              className="bg-white text-black py-1 rounded-2xl w-20"
             >
               Sign Up
             </Link>
-            <button type="submit" className="bg-blue-500 py-1 px-2 rounded-2xl">
+            <button type="submit" className="bg-blue-500 py-1 rounded-2xl w-20">
               Sign In
             </button>
           </div>
--- circle-jp/app/auth/sign-up/page.tsx
+++ circle-jp/app/auth/sign-up/page.tsx
@@ -13,42 +13,47 @@ function SignUp() {
       <main className="border border-inherit w-[600px] h-[370px] rounded-2xl p-6 z-10 backdrop-blur-xl space-y-10">
         <h4 className="text-center text-4xl">Sign Up</h4>
 
-        <form action="" className="mx-auto space-y-3 w-[400px]">
-          <div>
-            <legend>username</legend>
-            <input
-              type="text"
-              name="username"
-              className="border w-[400px] rounded-xl"
-            />
+        <form
+          action=""
+          className="mx-auto w-[400px] h-60 flex flex-col justify-between"
+        >
+          <div className="space-y-3">
+            <div>
+              <legend>Username</legend>
+              <input
+                type="text"
+                name="username"
+                className="border w-[400px] rounded-xl"
+              />
+            </div>
+
+            <div>
+              <legend>Email</legend>
+              <input
+                type="email"
+                name="email"
+                className="border w-[400px] rounded-xl"
+              />
+            </div>
+
+            <div>
+              <legend>Password</legend>
+              <input
+                type="password"
+                name="password"
+                className="border w-[400px] rounded-xl"
+              />
+            </div>
           </div>
 
-          <div>
-            <legend>Email</legend>
-            <input
-              type="email"
-              name="email"
-              className="border w-[400px] rounded-xl"
-            />
-          </div>
-
-          <div>
-            <legend>Password</legend>
-            <input
-              type="password"
-              name="password"
-              className="border w-[400px] rounded-xl"
-            />
-          </div>
-
-          <div className="flex justify-between items-center pt-5 text-[16px]">
+          <div className="flex justify-between items-center pt-5 text-[17px] text-center">
             <Link
               href={"/auth/sign-in"}
-              className="bg-white text-black py-1 px-2 rounded-2xl"
+              className="bg-white text-black py-1 rounded-2xl w-20"
             >
               Sign In
             </Link>
-            <button type="submit" className="bg-blue-500 py-1 px-2 rounded-2xl">
+            <button type="submit" className="bg-blue-500 py-1 rounded-2xl w-20">
               Sign Up
             </button>
           </div>
